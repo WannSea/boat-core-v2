7a997264053239166928667919d85da6e728e172
Kolja Kieslich <kolja.kieslich@gmail.com>
2024-01-07 14:16:21 +0100

    Fixed websocket

--- src/transport/web_socket_server.rs
+++ src/transport/web_socket_server.rs
@@ -1,14 +1,34 @@
 use futures::{StreamExt, SinkExt};
 use log::info;
-use tokio::{net::{TcpListener, TcpStream}, sync::broadcast};
+use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::tungstenite::Message;
-use wannsea_types::BoatCoreMessage;
+use wannsea_types::{BoatCoreMessage, MetricId};
 
 use crate::{SETTINGS, helper::MetricSender};
 
 pub struct WebSocketServer {
     message_bus: MetricSender
 } 
+
+fn get_json_value(msg: &BoatCoreMessage) -> String {
+    match msg.clone().value.unwrap() {
+        wannsea_types::boat_core_message::Value::Double(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Float(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Int32(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Int64(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Uint32(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Uint64(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Sint32(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Sint64(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Fixed32(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Fixed64(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Sfixed32(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Sfixed64(x) => x.to_string(),
+        wannsea_types::boat_core_message::Value::Bool(x) => if x { "\"true\"".to_string() } else { "\"false\"".to_string() },
+        wannsea_types::boat_core_message::Value::String(x) => x,
+        wannsea_types::boat_core_message::Value::Bytes(_) => "Bytes unsupported!".to_string(),
+    }
+}
 
 fn handle_client(stream: TcpStream, addr: String, metric_bus: MetricSender) {
     tokio::spawn(async move {
@@ -23,9 +43,8 @@ fn handle_client(stream: TcpStream, addr: String, metric_bus: MetricSender) {
         let mut receiver = metric_bus.subscribe();
         loop {
             let msg = receiver.recv().await.unwrap();
-            // if let Ok(data) = msg.get_json_repr() {
-            //     out.send(Message::text(data)).await.unwrap();
-            // }
+            let json = format!("{{ \"ts\": {}, \"cat\": \"{}\", \"value\": {} }}", msg.timestamp, MetricId::from_repr(msg.cat as usize).unwrap(), get_json_value(&msg));
+            out.send(Message::Text(json)).await.unwrap();
         }
     });
 }
