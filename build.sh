e5a4c55a2906361f32e30d0d0722a32cbfc6f7f0
kkieslich <kolja.kieslich@gmail.com>
2024-05-22 20:32:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 cargo update
 cargo build --release --target aarch64-unknown-linux-gnu
 
-VERSION=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')
+# Dont use docker because of sysinfo deps
+#VERSION=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')
 
-docker build -t ghcr.io/wannsea/boat-core-v2:${VERSION} -t ghcr.io/wannsea/boat-core-v2:latest  .
-docker push ghcr.io/wannsea/boat-core-v2 --all-tags
+#docker build -t ghcr.io/wannsea/boat-core-v2:${VERSION} -t ghcr.io/wannsea/boat-core-v2:latest  .
+#docker push ghcr.io/wannsea/boat-core-v2 --all-tags
